8169c4ccf1cba2ae14c24c1fc9f699a2c702be99
Adones Melo <adones.n.m@outlook.com>
2025-11-02 09:07:09 -0300

    Corrigindo a entrada de dados

--- escola/views.py
+++ escola/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 def estudantes(request):
     if request.method == 'GET':
         estudante = {
-            'id': '1',
+            'id': 1,
             'nome': 'Adones',
         }
 

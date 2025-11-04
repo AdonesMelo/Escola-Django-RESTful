ce0f762ce9caad32a7cb402022c063f03823ee01
Adones Melo <adones.n.m@outlook.com>
2025-11-04 18:32:28 -0300

    Trabalhando com Regex

--- escola/validators.py
+++ escola/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_invalido(cpf):
     return len(cpf) != 11
 
@@ -5,4 +7,8 @@ def nome_invalido(nome):
     return not nome.isalpha()
 
 def celular_invalidor(celular):
-    return len(celular) != 13
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}' # modelo do número do celular 86 99999-9999
+    resposta = re.findall(modelo, celular)
+    #print(resposta)
+
+    return not resposta

1a70082eea6c03b917248c73ea975e6d43a6a078
Adones Melo <adones.n.m@outlook.com>
2025-11-04 13:46:58 -0300

    Adicionando recursos de listar matriculas

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -8,7 +8,10 @@ router.register('estudantes', EstudanteViewSet, basename='Estudantes')
 router.register('cursos', CursoViewSet, basename='Cursos')
 router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas/', ListaMatriculaEstudante.as_view()), # somente leitura
+    path('cursos/<int:pk>/matriculas/', ListaMatriculaCurso.as_view()), # somente leitura
 ]

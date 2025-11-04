7c6ed1ea92f39fb5bfb4999a7013930a7bed5631
Adones Melo <adones.n.m@outlook.com>
2025-11-04 20:08:14 -0300

    Importando validações

--- escola/serializers.py
+++ escola/serializers.py
@@ -9,11 +9,11 @@ class EstudanteSerializer(serializers.ModelSerializer):
 
     def validate(self, dados):
         if cpf_invalido(dados['cpf']):
-            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 digitos!'})
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter uma valor válido!'})
         if nome_invalido(dados['nome']):
             raise serializers.ValidationError({'nome': 'O nome só pode ter letras!'})
         if celular_invalidor(dados['celular']):
-            raise serializers.ValidationError({'celular': 'O celular deve ter 13 digitos!'})
+            raise serializers.ValidationError({'celular': 'O celular deve seguir o modelo: 86 99999-9999(contendo espaço e traço)'})
         
         return dados
 

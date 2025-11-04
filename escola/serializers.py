66eded7c021ca05aa6cccecd88ee824c175d508b
Adones Melo <adones.n.m@outlook.com>
2025-11-04 12:51:38 -0300

    Criando os recursos de matrícula

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from escola.models import Estudante, Curso
+from escola.models import Estudante, Curso, Matricula
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
+        fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular'] 
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []

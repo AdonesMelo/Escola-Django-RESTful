052e80bf099d05c0210b02c1039de1baa89c71da
Adones Melo <adones.n.m@outlook.com>
2025-11-04 13:11:34 -0300

    Adicionando Serializer de listar matriculas: Estudante e curso

--- escola/serializers.py
+++ escola/serializers.py
@@ -15,3 +15,21 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = []
+
+class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    nome_estudante = serializers.ReadOnlyField(source='estudante.curso')
+
+    class Meta:
+        model = Matricula
+        fields = ['nome_estudante']

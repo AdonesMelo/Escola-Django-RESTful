d67b932b2e3436bfeed960d5f3ec6446d85b4c20
Adones Melo <adones.n.m@outlook.com>
2025-11-11 15:31:24 -0300

    Configurando o Swagger

--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação API",
+      default_version='v1',
+      description="Documentação API Escola",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename='Estudantes')
@@ -14,4 +28,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('estudantes/<int:pk>/matriculas/', ListaMatriculaEstudante.as_view()), # somente leitura
     path('cursos/<int:pk>/matriculas/', ListaMatriculaCurso.as_view()), # somente leitura
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

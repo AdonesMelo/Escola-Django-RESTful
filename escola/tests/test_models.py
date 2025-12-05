e25b735fd0f53f753082955fcba1c9f08e25315c
Adones Melo <adones.n.m@outlook.com>
2025-12-05 16:13:46 -0300

    Testando o model: Estudate

--- escola/tests/test_models.py
+++ escola/tests/test_models.py
@@ -2,5 +2,21 @@ from django.test import TestCase
 from escola.models import Estudante
 
 class ModelEstudanteTestCase(TestCase):
-    def teste_falha(self):
-        self.fail('Teste falhou :(')
+    # def teste_falha(self):
+    #     self.fail('Teste falhou :(')
+    def setUp(self):
+       self.estudante = Estudante.objects.create(
+           nome = 'Teste Model',
+           email = 'testemodelo@teste.com',
+           cpf = '74299631021',
+           data_nascimento = '2023-01-01',
+           celular = '99 99999-9999'
+       )
+
+    def test_verifica_atributos_de_estudante(self):
+        '''Teste que verifica os atributos do modelo de estudate'''
+        self.assertEqual(self.estudante.nome, 'Teste Model')
+        self.assertEqual(self.estudante.email, 'testemodelo@teste.com')
+        self.assertEqual(self.estudante.cpf, '74299631021')
+        self.assertEqual(self.estudante.data_nascimento, '2023-01-01')
+        self.assertEqual(self.estudante.celular, '99 99999-9999')

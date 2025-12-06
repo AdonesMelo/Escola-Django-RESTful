0256ae8c65f7c496f7ae98b6b7472aa282cbf1eb
Adones Melo <adones.n.m@outlook.com>
2025-12-06 15:16:02 -0300

    Testando serializer: Curso e Matrícula

--- escola/tests/test_serializers.py
+++ escola/tests/test_serializers.py
@@ -27,3 +27,61 @@ class SerializerEstudanteTestCase(TestCase):
         self.assertEqual(dados['cpf'], self.estudante.cpf)
         self.assertEqual(dados['data_nascimento'], self.estudante.data_nascimento)
         self.assertEqual(dados['celular'], self.estudante.celular)
+
+class SerializerCursoTestCase(TestCase):
+    def setUp(self):
+        self.curso = Curso(
+            codigo = 'PY2026',
+            descricao = 'Python',
+            nivel = 'A'
+        )
+
+        self.serializer_curso = CursoSerializer(instance=self.curso)
+
+    def test_verifica_campos_serializados_do_curso(self):
+        '''Teste que verifica os campos que estão sendo serializados do curso'''
+        dados = self.serializer_curso.data
+        self.assertEqual(set(dados.keys()), set(['id', 'codigo', 'descricao', 'nivel']))
+    
+    def test_verifica_conteudo_dos_campos_serializados_do_curso(self):
+        '''Teste que verifica o conteúdo dos campos que estão sendo serializados do curso'''
+        dados = self.serializer_curso.data
+        self.assertEqual(dados['codigo'], self.curso.codigo)
+        self.assertEqual(dados['descricao'], self.curso.descricao)
+        self.assertEqual(dados['nivel'], self.curso.nivel)
+
+class SerializerMatriculaTestCase(TestCase):
+    def setUp(self):
+        # Cria as dependências necessárias (Foreign Keys)
+        self.estudante = Estudante.objects.create(
+            nome='Aluno Teste',
+            email='aluno@teste.com',
+            cpf='85191988051',
+            data_nascimento='2000-01-01',
+            celular='11 99999-9999'
+        )
+        self.curso = Curso.objects.create(
+            codigo='TDD01',
+            descricao='Curso de Testes com Django',
+            nivel='A'
+        )
+        # Cria a matrícula alvo do teste
+        self.matricula = Matricula.objects.create(
+            estudante=self.estudante,
+            curso=self.curso,
+            periodo='M'
+        )
+
+        self.serializer_matricula = MatriculaSerializer(instance=self.matricula)
+
+    def test_verifica_campos_serializados_da_matricula(self):
+        '''Teste que verifica os campos que estão sendo serializados da matrícula'''
+        dados = self.serializer_matricula.data
+        self.assertEqual(set(dados.keys()), set(['id', 'estudante', 'curso', 'periodo']))
+    
+    def test_verifica_conteudo_dos_campos_serializados_da_matricula(self):
+        '''Teste que verifica o conteúdo dos campos que estão sendo serializados do matrícula'''
+        dados = self.serializer_matricula.data
+        self.assertEqual(dados['estudante'], self.matricula.estudante.id)
+        self.assertEqual(dados['curso'], self.matricula.curso.id)
+        self.assertEqual(dados['periodo'], self.matricula.periodo)

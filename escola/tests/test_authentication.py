c010a5008085455d42bd979fa163d31cf924ab89
Adones Melo <adones.n.m@outlook.com>
2025-12-09 14:09:43 -0300

    Testes de autenticação na requisição GET

--- escola/tests/test_authentication.py
+++ escola/tests/test_authentication.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
 
 class AuthenticationUserTesCase(APITestCase):
     def setUp(self):
         self.ususario = User.objects.create_superuser(username='admin', password='admin')
+        self.url = reverse('Estudantes-list')
     
     def test_autenticacao_user_com_credenciais_corretas(self):
         '''Teste que verifica a autenticação de um user com as crendencias corretas'''
@@ -20,3 +23,14 @@ class AuthenticationUserTesCase(APITestCase):
         '''Teste que verifica a autenticação com password incorreta'''
         usuario = authenticate(username='admin', password='adin')
         self.assertFalse((usuario is not None) and usuario.is_authenticated)
+
+    def test_requisicao_get_autorizado(self):
+        '''Teste que verifica uma requisição GET autorizada'''
+        self.client.force_authenticate(self.ususario)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_get_autorizado(self):
+        '''Teste que verifica uma requisição GET não autorizada'''
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
